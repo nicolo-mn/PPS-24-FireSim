f018a036c8c45622b4caeae24050cefdabbf3178
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2025-07-31 11:27:38 +0200

    test: add periodic callback invocation test for SimUpdater

---
+++ src/test/scala/it/unibo/firesim/controller/SimUpdaterSpec.scala
@@ -0,0 +1,28 @@
+package it.unibo.firesim.controller
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Seconds, Span}
+
+import java.util.concurrent.atomic.AtomicInteger
+
+class SimUpdaterSpec extends AnyFlatSpec with Matchers with Eventually {
+
+  implicit override val patienceConfig =
+    PatienceConfig(timeout = Span(2, Seconds), interval = Span(50, Millis))
+
+  "SimUpdater" should "invoke callback periodically when running" in {
+    val counter = new AtomicInteger(0)
+    val updater = new SimUpdater(50)  // tick ogni 50 ms
+    updater.setUpdateCallback(() => counter.incrementAndGet())
+    updater.start()
+
+    // aspettiamo che almeno 3 invocazioni si verifichino
+    eventually {
+      counter.get() should be >= 3
+    }
+
+    updater.stop()
+  }
+}
