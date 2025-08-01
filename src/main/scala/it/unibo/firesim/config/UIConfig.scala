4fbb6c92293ec115001636e564724e427fcc8803
Nicolò Monaldini <nicolo.monaldini@gmail.com>
2025-08-01 15:11:51 +0200

    chore: add UI config values

---
+++ src/main/scala/it/unibo/firesim/config/UIConfig.scala
@@ -0,0 +1,35 @@
+package it.unibo.firesim.config
+
+import java.awt.Color
+
+object UIConfig:
+  val minWidth: Int = 300
+  val minHeight: Int = 300
+  val defaultWidth: Int = 500
+  val defaultHeight: Int = 500
+  val defaultGridSize: Int = 10
+  val minHumidity: Int = 0
+  val maxHumidity: Int = 100
+  val defaultHumidity: Int = 50
+  val minTemperature: Int = -20
+  val maxTemperature: Int = 50
+  val defaultTemperature: Int = 20
+  val minWindDirection: Int = 0
+  val maxWindDirection: Int = 360
+  val defaultWindDirection: Int = 90
+  val minWindIntensity: Int = 0
+  val maxWindIntensity: Int = 150
+  val defaultWindIntensity: Int = 20
+  val humidityLabelText: String = "Current humidity: "
+  val humidityUnit: String = " %"
+  val temperatureLabelText: String = "Current temperature: "
+  val temperatureUnit: String = " °C"
+  val windDirectionLabelText: String = "Current wind angle: "
+  val windDirectionUnit: String = "°"
+  val windIntensityLabelText: String = "Current wind speed: "
+  val windIntensityUnit: String = " km/s"
+  val backgroundColor: Color = Color.white
+  val fireSoilStr: String = "Fire\uD83D\uDD25"
+  val grassSoilStr: String = "Grass\uD83C\uDF31"
+  val forestSoilStr: String = "Forest\uD83C\uDF32"
+  val emptySoilStr: String = "Empty\uD83D\uDED1"
