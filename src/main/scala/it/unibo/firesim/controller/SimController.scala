73d023617b073d4b63145c976e468d6f39528855
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2025-08-01 17:22:59 +0200

    feat: add logic to recieve command for simulation state

--- src/main/scala/it/unibo/firesim/controller/SimController.scala
+++ src/main/scala/it/unibo/firesim/controller/SimController.scala
@@ -4,9 +4,9 @@ import it.unibo.firesim.debug.MockSimModel
 import it.unibo.firesim.debug.SimParams
 
 class SimController(
-                     model: MockSimModel,
-                     updater: SimUpdater
-                   ) {
+    model: MockSimModel,
+    updater: SimUpdater
+):
 
   private val mutex = new Object
 
@@ -20,15 +20,25 @@ class SimController(
   def setTemperature(temp: Double): Unit = updateModel(model.setTemperature, temp)
   def setHumidity(humidity: Double): Unit = updateModel(model.setHumidity, humidity)
 
+  def getSimParams: SimParams = mutex.synchronized {
+    model.getSimParams
+  }
+
   def generateMap(width: Int, height: Int): Unit = ???
   def placeFire(pos: (Int, Int)): Unit = ???
   def placeBarrier(startPos: (Int, Int), finishPos: (Int, Int)): Unit = ???
 
-  def startSimulation(): Unit = ???
-  def pauseResumeSimulation(): Unit = ???
-  def stopSimulation(): Unit = ???
+  def startSimulation(): Unit = updater.start()
+  def pauseResumeSimulation(): Unit = updater.pauseResume()
+  def stopSimulation(): Unit = updater.stop()
 
-  def getSimParams: SimParams = mutex.synchronized { model.getSimParams }
+  def setView(): Unit =
+    updater.setUpdateCallback(() => onTick())
 
-}
-
+  private def onTick(): Unit = mutex.synchronized {
+    try {
+    } catch {
+      case ex: Exception =>
+        ex.printStackTrace()
+    }
+  }
