f38487dbe0c0ce175b3e46c2e2d50044d17af346
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2025-08-06 14:54:26 +0200

    feat: added burnt and station type of soil in UIConfig and CellViewType

--- src/main/scala/it/unibo/firesim/controller/CellViewType.scala
+++ src/main/scala/it/unibo/firesim/controller/CellViewType.scala
@@ -1,12 +1,14 @@
 package it.unibo.firesim.controller
 
-import it.unibo.firesim.config.UIConfig.{emptySoilStr, fireSoilStr, forestSoilStr, grassSoilStr}
+import it.unibo.firesim.config.UIConfig.{burntSoilStr, emptySoilStr, fireSoilStr, forestSoilStr, grassSoilStr, stationSoilStr}
 
 enum CellViewType(val soilType: String):
   case Fire extends CellViewType(fireSoilStr)
   case Grass extends CellViewType(grassSoilStr)
   case Forest extends CellViewType(forestSoilStr)
   case Empty extends CellViewType(emptySoilStr)
+  case Burnt extends CellViewType(burntSoilStr)
+  case Station extends CellViewType(stationSoilStr)
 
 object CellViewType:
   def fromString(s: String): Option[CellViewType] =
