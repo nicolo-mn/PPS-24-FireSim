3ad3f415f76d5b928aad26bd936ca5f93bfc3d72
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2025-08-08 14:55:44 +0200

    feat: add FireSpread logic

---
+++ src/main/scala/it/unibo/firesim/model/fire/FIreSpread.scala
@@ -0,0 +1,25 @@
+package it.unibo.firesim.model.fire
+
+import it.unibo.firesim.model.{Matrix, SimParams}
+import it.unibo.firesim.model.cell.CellType
+
+
+def fireSpread(matrix: Matrix, params: SimParams, currentCycle: Int)
+              (using prob: ProbabilityCalc, burn: BurnDurationPolicy, rand: RandomProvider): Matrix =
+  matrix.zipWithIndex.map { (row, r) =>
+    row.zipWithIndex.map { (cell, c) =>
+      cell.cellType match
+        case Burning(start) =>
+          if currentCycle.hasBurnedEnough(start) then
+            cell.copy(cellType = CellType.Burnt)
+          else cell
+
+        case cellType if cellType.isFlammable =>
+          val hasBurningNeighbor = matrix.burningNeighbors(r, c).nonEmpty
+          if hasBurningNeighbor && rand() < prob(cell, params, r, c, matrix)
+          then cell.copy(cellType = CellType.Burning(currentCycle))
+          else cell
+
+        case _ => cell
+    }
+  }
