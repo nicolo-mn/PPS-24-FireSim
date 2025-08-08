b637a7c75e78d566a7c25d45b1e54da390412519
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2025-08-08 16:29:51 +0200

    refactor: add new test and refactor

--- src/test/scala/it/unibo/firesim/model/fire/FireSpreadTest.scala
+++ src/test/scala/it/unibo/firesim/model/fire/FireSpreadTest.scala
@@ -1,25 +1,18 @@
 package it.unibo.firesim.model.fire
 
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import it.unibo.firesim.model.{Matrix, SimParams}
 import it.unibo.firesim.model.cell.{Cell, CellType}
 
-class FireSpreadTest extends AnyFunSuite with Matchers:
+class FireSpreadTest extends AnyFlatSpec with Matchers:
 
-  test(
-    "a flammable cell becomes burning with high probability and burning neighbor"
-  ):
+  "fireSpread" should "turn flammable cells into burning with high probability and a burning neighbor" in {
     val matrix: Matrix = Vector(
       Vector(Cell(0, 0, CellType.Grass), Cell(0, 1, CellType.Burning(0))),
       Vector(Cell(1, 0, CellType.Grass), Cell(1, 1, CellType.Grass))
     )
-    val params = SimParams(
-      windSpeed = 0.0,
-      windAngle = 0.0,
-      temperature = 30.0,
-      humidity = 10.0
-    )
+    val params = SimParams(0.0, 0.0, 30.0, 10.0)
 
     given prob: ProbabilityCalc = (_, _, _, _, _) => 1.0
     given burn: BurnDurationPolicy = (_, _) => false
@@ -28,11 +21,10 @@ class FireSpreadTest extends AnyFunSuite with Matchers:
     val result = fireSpread(matrix, params, 1)
     result(0)(0).cellType shouldBe CellType.Burning(1)
     result(1)(0).cellType shouldBe CellType.Burning(1)
+  }
 
-  test("a burning cell becomes burnt after enough cycles"):
-    val matrix: Matrix = Vector(
-      Vector(Cell(0, 0, CellType.Burning(0)))
-    )
+  it should "turn burning cells into burnt after 3 cycles" in {
+    val matrix: Matrix = Vector(Vector(Cell(0, 0, CellType.Burning(0))))
     val params = SimParams(0, 0, 20, 50)
 
     given prob: ProbabilityCalc = (_, _, _, _, _) => 0.0
@@ -44,8 +36,9 @@ class FireSpreadTest extends AnyFunSuite with Matchers:
       params,
       3
     )(using prob, burn, rand)(0)(0).cellType shouldBe CellType.Burnt
+  }
 
-  test("a flammable cell does not burn if probability is zero"):
+  it should "not burn a cell if probability is zero" in {
     val matrix: Matrix = Vector(
       Vector(Cell(0, 0, CellType.Grass), Cell(0, 1, CellType.Burning(0)))
     )
@@ -60,15 +53,13 @@ class FireSpreadTest extends AnyFunSuite with Matchers:
       params,
       1
     )(using prob, burn, rand)(0)(0).cellType shouldBe CellType.Grass
+  }
 
-  test(
-    "defaultProbabilityCalc gives higher probability for forest than grass"
-  ) {
+  "defaultProbabilityCalc" should "give higher probability for forest than grass" in {
     val forest = Cell(0, 0, CellType.Forest)
     val grass = Cell(0, 1, CellType.Grass)
     val dummyMatrix = Vector.empty
-    val params =
-      SimParams(windSpeed = 0, windAngle = 0, temperature = 30, humidity = 20)
+    val params = SimParams(0, 0, 30, 20)
 
     val forestProb = defaultProbabilityCalc(forest, params, 0, 0, dummyMatrix)
     val grassProb = defaultProbabilityCalc(grass, params, 0, 1, dummyMatrix)
@@ -78,17 +69,18 @@ class FireSpreadTest extends AnyFunSuite with Matchers:
     grassProb should be >= 0.0
   }
 
-  test("defaultBurnDuration returns true after 3 cycles") {
+  "defaultBurnDuration" should "return true only after 3 cycles" in {
     defaultBurnDuration(5, 8) shouldBe true
     defaultBurnDuration(5, 7) shouldBe false
+
   }
 
-  test("defaultRandomProvider returns value between 0.0 and 1.0") {
+  "defaultRandomProvider" should "return a value between 0.0 and 1.0" in {
     val value = defaultRandomProvider()
     value should (be >= 0.0 and be <= 1.0)
   }
 
-  test("windAndHumidityAdjusted applies penalty if humidity is high") {
+  "windAndHumidityAdjusted" should "apply penalty if humidity is high" in {
     val cell = Cell(0, 0, CellType.Forest)
     val matrix = Vector(Vector(cell.copy(cellType = CellType.Burning(0))))
     val lowHumidity = SimParams(1, 0, 30, 30)
@@ -98,4 +90,32 @@ class FireSpreadTest extends AnyFunSuite with Matchers:
     val high = windAndHumidityAdjusted(cell, highHumidity, 0, 0, matrix)
 
     high should be < low
+  }
+
+  "directionalWindProbabilityDynamic" should "boost probability if cell in wind direction is burning" in {
+    val params = SimParams(1.0, 0.0, 25.0, 0.0)
+    val matrix: Matrix = Vector(
+      Vector(
+        Cell(0, 0, CellType.Grass),
+        Cell(0, 1, CellType.Grass),
+        Cell(0, 2, CellType.Grass)
+      ),
+      Vector(
+        Cell(1, 0, CellType.Grass),
+        Cell(1, 1, CellType.Grass),
+        Cell(1, 2, CellType.Burning(0))
+      ),
+      Vector(
+        Cell(2, 0, CellType.Grass),
+        Cell(2, 1, CellType.Grass),
+        Cell(2, 2, CellType.Grass)
+      )
+    )
+    val base: ProbabilityCalc = (_, _, _, _, _) => 0.4
+    val windAdjusted = directionalWindProbabilityDynamic(base)
+
+    val boosted = windAdjusted(matrix(1)(1), params, 1, 1, matrix)
+
+    boosted should be > 0.4
+    boosted should be <= 1.0
   }
