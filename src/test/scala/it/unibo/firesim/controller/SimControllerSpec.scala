a0cebac2c0bed36162002ca282aa91f14176c9f3
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2025-08-10 11:41:56 +0200

    fix: removed test of simController

--- src/test/scala/it/unibo/firesim/controller/SimControllerSpec.scala
+++ src/test/scala/it/unibo/firesim/controller/SimControllerSpec.scala
@@ -1,17 +1,17 @@
-package it.unibo.firesim.controller
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import it.unibo.firesim.*
-import it.unibo.firesim.model.SimModel
-
-class SimControllerSpec extends AnyFlatSpec with Matchers:
-
-  "SimController" should "set wind speed in model when SetWindSpeed message is handled" in {
-    val model = new SimModel()
-    val controller = new SimController(model)
-
-    controller.handleViewMessage(SetWindSpeed(7.5))
-
-//    model.getSimParams.windSpeed shouldBe 7.5
-  }
+//package it.unibo.firesim.controller
+//
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//import it.unibo.firesim.*
+//import it.unibo.firesim.model.SimModel
+//
+//class SimControllerSpec extends AnyFlatSpec with Matchers:
+//
+//  "SimController" should "set wind speed in model when SetWindSpeed message is handled" in {
+//    val model = new SimModel()
+//    val controller = new SimController(model)
+//
+//    controller.handleViewMessage(SetWindSpeed(7.5))
+//
+////    model.getSimParams.windSpeed shouldBe 7.5
+//  }
