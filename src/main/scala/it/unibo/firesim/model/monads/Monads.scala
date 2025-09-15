e1324110e27cb5813b9f0bb1cbeafe5057e3b4ed
Nicolò Monaldini <nicolo.monaldini@gmail.com>
2025-09-16 01:01:36 +0200

    docs: add scaladoc for monads package

--- src/main/scala/it/unibo/firesim/model/monads/Monads.scala
+++ src/main/scala/it/unibo/firesim/model/monads/Monads.scala
@@ -1,10 +1,18 @@
 package it.unibo.firesim.model.monads
 
+/** A computational package for elements of type A. */
 object Monads:
 
+  /** Monad interface.
+   *
+   * Defines unit and composition operations.
+   */
   trait Monad[M[_]]:
+    /** Wraps a value. */
     def unit[A](a: A): M[A]
 
     extension [A](m: M[A])
+      /** Chains computations. */
       def flatMap[B](f: A => M[B]): M[B]
+      /** Map derived operation. */
       def map[B](f: A => B): M[B] = m.flatMap(a => unit(f(a)))
