f279b5659dea8f31f46cd4a397af46cf3091e119
Nicolò Monaldini <nicolo.monaldini@gmail.com>
2025-09-15 20:09:49 +0200

    fix: FireFighterDSLTest

--- src/test/scala/it/unibo/firesim/model/firefighters/builder/FireFighterDSLTest.scala
+++ src/test/scala/it/unibo/firesim/model/firefighters/builder/FireFighterDSLTest.scala
@@ -36,20 +36,17 @@ class FireFighterDSLTest extends AnyFlatSpec with Matchers:
       withRay(1)
       stationedIn(1, 1)
 
-    firefighter1.neighborsInRay should contain theSameElementsInOrderAs Seq((
-      0,
-      0
+    firefighter1.neighborsInRay should be(Set((0, 0)))
+
+    firefighter2.neighborsInRay should be(Set(
+      (-1, -1),
+      (-1, 0),
+      (-1, 1),
+      (0, -1),
+      (0, 0),
+      (0, 1),
+      (1, -1),
+      (1, 0),
+      (1, 1)
     ))
-    firefighter2.neighborsInRay should contain theSameElementsInOrderAs
-      Seq(
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 0),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1)
-      )
   }
