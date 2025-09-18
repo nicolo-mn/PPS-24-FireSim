240f4deb83eda471f46bf7447a2c8cca90f2f1e0
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2025-09-18 15:26:59 +0200

    refactor: correct bug for place cell fire

--- src/main/scala/it/unibo/firesim/controller/SimController.scala
+++ src/main/scala/it/unibo/firesim/controller/SimController.scala
@@ -187,6 +187,7 @@ class SimController(
             simView.setViewMap(model.generateMap(height, width)
               .flatten.map(CellTypeConverter.toView))
             mapGenerated = true
+            currentGeneration = model.getCurrentCycle
           else lock.wait()
       }
 
