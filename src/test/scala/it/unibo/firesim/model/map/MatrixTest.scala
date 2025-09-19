4e17cca6bac386b0e9262def7177321d3dbc9f3b
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2025-09-19 09:39:08 +0200

    test: improved Matrix test

--- src/test/scala/it/unibo/firesim/model/map/MatrixTest.scala
+++ src/test/scala/it/unibo/firesim/model/map/MatrixTest.scala
@@ -6,46 +6,36 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class MatrixTest extends AnyFlatSpec with Matchers:
+
+  private val matrix = Vector.tabulate(3, 3)((row, col) => Forest)
+
   "Matrix" should "return correct number of rows" in {
-    val matrix =
-      Vector.tabulate(3, 3)((row, col) => Forest)
     matrix.rows should be(3)
   }
 
   it should "return correct number of cols" in {
-    val matrix =
-      Vector.tabulate(3, 3)((row, col) => Forest)
     matrix.cols should be(3)
   }
 
   it should "return the correct cell for each position" in {
-    val matrix =
-      Vector.tabulate(3, 3)((row, col) => Forest)
     matrix(0)(0) should be(Forest)
   }
 
   it should "update cells correctly" in {
-    val matrix = Vector.tabulate(3, 3)((row, col) =>
-      Forest
-    ).update(0, 0, Station)
-    matrix(0)(0) should be(Station)
+    val updated = matrix.update(0, 0, Station)
+    updated(0)(0) should be(Station)
   }
 
   it should "get the positions of cells with a certain cell type" in {
-    val matrix = Vector.tabulate(3, 3)((row, col) =>
-      Forest
-    ).update(
-      0,
-      0,
-      Station
-    ).update(1, 1, Station).update(2, 2, Station)
-    matrix.positionsOf(Station) should be(Seq((0, 0), (1, 1), (2, 2)))
+    val updated = matrix
+      .update(0, 0,Station)
+      .update(1, 1, Station)
+      .update(2, 2, Station)
+    updated.positionsOf(Station) should be(Seq((0, 0), (1, 1), (2, 2)))
   }
 
   it should "get the positions of cells with a certain problematic cell type" in {
-    val matrix = Vector.tabulate(3, 3)((row, col) =>
-      Forest
-    ).update(
+    val updated = matrix.update(
       0,
       0,
       Burning(10, Ignition, Grass)
@@ -54,17 +44,15 @@ class MatrixTest extends AnyFlatSpec with Matchers:
       1,
       Burning(1, Ignition, Grass)
     ).update(2, 2, Burning(3, Ignition, Grass))
-    matrix.positionsOf(Burning(
+    updated.positionsOf(Burning(
       10,
       Ignition,
       Grass
     )) should be(Seq((0, 0)))
-    matrix.positionsOfBurning() should be(Seq((0, 0), (1, 1), (2, 2)))
+    updated.positionsOfBurning() should be(Seq((0, 0), (1, 1), (2, 2)))
   }
 
   it should "get the positions of the neighboring cells" in {
-    val matrix = Vector.tabulate(3, 3)((row, col) => Forest)
-
     matrix.neighbors(1, 1) should contain allElementsOf Seq(
       (0, 0),
       (1, 0),
