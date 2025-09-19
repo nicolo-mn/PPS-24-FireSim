b48767592314b6e85ca349f087391cacd7f328c2
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2025-09-19 15:31:37 +0200

    style: add explicit type in Config

--- src/main/scala/it/unibo/firesim/config/Config.scala
+++ src/main/scala/it/unibo/firesim/config/Config.scala
@@ -2,19 +2,19 @@ package it.unibo.firesim.config
 
 object Config:
   val fireFighterRay: Int = 3
-  val distanceFromStationWeight = 0.6
-  val distanceFromPosWeight = 0.4
-  val correctionThreshold = 0.9
-  val lakeGrowthProbability = 0.8
-  val forestGrowthProbability = 0.7
-  val grassGrowthProbability = 0.8
+  val distanceFromStationWeight: Double = 0.6
+  val distanceFromPosWeight: Double = 0.4
+  val correctionThreshold: Double = 0.9
+  val lakeGrowthProbability: Double = 0.8
+  val forestGrowthProbability: Double = 0.7
+  val grassGrowthProbability: Double = 0.8
 
   // These ratios are intended to be multiplied by the mean between the number of rows and cols
-  val lakeSeedFrequency = 0.01
-  val forestSeedFrequency = 1.8
-  val stationSeedsFrequency = 0.02
-  val forestFireSeedFrequency = 0.01
-  val grassFireSeedFrequency = 0.01
+  val lakeSeedFrequency: Double = 0.01
+  val forestSeedFrequency: Double = 1.8
+  val stationSeedsFrequency: Double = 0.02
+  val forestFireSeedFrequency: Double = 0.01
+  val grassFireSeedFrequency: Double = 0.01
 
   val minLakeSizeRatio: Double = 1.5
   val maxLakeSizeRatio: Double = 4.0
@@ -23,42 +23,42 @@ object Config:
   val minGrassSizeRatio: Double = 0.1
   val maxGrassSizeRatio: Double = 0.5
 
-  val minProbability = 0.0
-  val maxProbability = 1.0
+  val minProbability: Double = 0.0
+  val maxProbability: Double = 1.0
 
   // value for wind
-  val grades = 360.0
+  val grades: Double = 360.0
   val halfSector: Double = grades / 2.0
-  val windNormalization = 25.0
-  val maxWindBoost = 1.0
-  val baseWindBoost = 1.0
-  val humidityPenalty = 0.7
-  val highHumidity = 85
+  val windNormalization: Double = 25.0
+  val maxWindBoost: Double = 1.0
+  val baseWindBoost: Double = 1.0
+  val humidityPenalty: Double = 0.7
+  val highHumidity: Int = 85
 
   // basic fire policy
-  val humidityMidpoint = 70.0
-  val humidityScale = 20.0
-  val temperatureMidpoint = 20.0
-  val temperatureScale = 5.0
-  val neighborInfluenceWeight = 0.05
-  val baseNeighborInfluence = 1.0
+  val humidityMidpoint: Double = 70.0
+  val humidityScale: Double = 20.0
+  val temperatureMidpoint: Double = 20.0
+  val temperatureScale: Double = 5.0
+  val neighborInfluenceWeight: Double = 0.05
+  val baseNeighborInfluence: Double = 1.0
 
   // fire stage
-  val ignitionProbabilityFactor = 0.3
-  val ignitionActivationThreshold = 0.3
-  val activeProbabilityFactor = 1.0
-  val activeActivationThreshold = 0.8
-  val smolderingProbabilityFactor = 0.2
-  val smolderingActivationThreshold = 1.0
+  val ignitionProbabilityFactor: Double = 0.3
+  val ignitionActivationThreshold: Double = 0.3
+  val activeProbabilityFactor: Double = 1.0
+  val activeActivationThreshold: Double = 0.8
+  val smolderingProbabilityFactor: Double = 0.2
+  val smolderingActivationThreshold: Double = 1.0
 
   // vegetation
-  val forestFlammability = 0.02
-  val grassFlammability = 0.05
-  val noFlammability = 0.0
-  val forestBurnDuration = 100
-  val grassBurnDuration = 70
-  val noBurnDuration = 0
+  val forestFlammability: Double = 0.02
+  val grassFlammability: Double = 0.05
+  val noFlammability: Double = 0.0
+  val forestBurnDuration: Int = 100
+  val grassBurnDuration: Int = 70
+  val noBurnDuration: Int = 0
 
   // water influence
-  val coastalEffectMaxRange = 4
-  val maxCoastalHumidityEffect = 0.5
+  val coastalEffectMaxRange: Int = 4
+  val maxCoastalHumidityEffect: Double = 0.5
